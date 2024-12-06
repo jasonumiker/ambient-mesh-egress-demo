4a8acd0e464395ff4b4708312a11d223b89fd86b
Jason Umiker <jason.umiker@gmail.com>
2024-12-06 13:15:15 +1100

    Added sleeps

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,12 @@
 # Install curl service in default
 kubectl apply -f curl.yaml
+sleep 10
 kubectl exec deploy/curl -- curl -sI http://httpbin.org/get -v
 
 # Set up ambient in the default namespace
 kubectl label namespace default istio.io/dataplane-mode=ambient
 /root/.istioctl/bin/istioctl waypoint apply --enroll-namespace --name egress-gateway --namespace default
+sleep 10
 
 # Add the serviceentry for httpbin.org
 kubectl apply -f serviceentry.yaml
